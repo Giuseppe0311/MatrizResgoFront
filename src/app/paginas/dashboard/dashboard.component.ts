dcf69beb98a6d1c3d47a33ba2a8918c12aaa4061
mcalderon <mcalderon>
2024-05-12 00:14:07 -0500

    se agrego el boton de evento

--- src/app/paginas/dashboard/dashboard.component.ts
+++ src/app/paginas/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CreareventoComponent } from './crearevento/crearevento.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CreareventoComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
